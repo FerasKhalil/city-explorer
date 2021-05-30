52111ebab9abe9c93f3535f74c1c23c1d5150373
Firas Hasan <x.firashasan@gmail.com>
2021-05-30 10:08:50 +0300

    test frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React  from 'react';
+
+export class App extends React.Component {
+
+
+getWeather = async () => {
+let serverRoute = process.env.REACT_APP_SERVER;
+  const url = `${serverRoute}/test`;
+  //we need to use axios to send it(request)
+  const testData= await axios.get(url);
+  console.log(testData.data);
+}
+
+  render() {
+    return (
+      <div>
+            <h1>the front end website </h1>
+            <p>to send a request from here(local react app) to our backend(local server)
+              then connect them together</p>  
+              <button onClick={this.getWeather}>get weather stats</button>      
+      </div>
+    )
+  }
 }
 
 export default App;
